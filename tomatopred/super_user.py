7b161613e3fe5c386415d6165a3baec121d15312
Lydu7979 <ludovic19sanne@yahoo.fr>
2021-12-27 09:49:26 +0100

    old timer

---
+++ tomatopred/super_user.py
@@ -0,0 +1,3 @@
+super_login_email = 'Samantha@yahoo.fr'
+super_pwd = 'Samantha45'
+super_pwd_hs = 'pbkdf2:sha256:260000$K8zjff7UsrivEOPa$787a332deb4a7cde841b304a6a3a0f2c611983f398c41a523787642c0f4f1a6d'
